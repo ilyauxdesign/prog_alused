ff127c612b16b2d6d4617fcee8bf1d04f1f704bc
ilya <ilyakalinkin@gmail.com>
2021-10-14 09:46:17 +0300

    added variables

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ two lines comment
 console.log('Hello World');
 console.log('this is ' + 'second line');
 console.log('Third line');
+
+let person = "Kerstike";
+let year = 2016;
+
+console.log( year + ' valiti ' + person + ' presidendiks.');
+
+document.getElementById("demo").innerHTML = year + ' valiti ' + person + ' presidendiks.';
